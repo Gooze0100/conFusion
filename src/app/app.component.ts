8f30ed4ca92268c1745f75d6c4e4c09215b34d55
Giedrius <kalniusg@gmail.com>
2021-12-19 23:03:28 +0200

    Components part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,19 @@ import { Component } from '@angular/core';
 // cia yra kitas dekoratorius
 @Component({
   // cia yra selectorius, jis browseryje atspindimas
+  // bus second component perduota
   selector: 'app-root',
   // cia define view is kur turi atspindeti
+  // specifies template file fir this specific file
   templateUrl: './app.component.html',
+  // this is inline application, we can use variables too
+  // template: `<h1>{{title}}</h1>`,
   // cia stilius koks bus taikomas komponentui
+  // cia galima tureti irgi inline code
   styleUrls: ['./app.component.scss'],
 })
 // mes cia paruosiame class ir ja naujame paskui
+// sitas komponentas gali buti exportuojamas i musu app module
 export class AppComponent {
   title = 'conFusion';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 // musu root component
 import { AppComponent } from './app.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatListModule } from '@angular/material/list';
+
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   // cia deklrajuoja view class tai cia aprasomos directive, pipes ir components
-  declarations: [AppComponent],
+  declarations: [AppComponent, MenuComponent],
   // cia eina importai, nuo kuriu bus priklausoma musu aplikacija, kai importuojam jas reikia cia reikia jas aprasyti nuo ko bus dependant
   imports: [
     BrowserModule,
@@ -19,6 +24,7 @@ import 'hammerjs';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
+    MatListModule,
   ],
   // providers specified visus servisus kuriuos sitas konkreciai komponentas naudosis
   providers: [],
