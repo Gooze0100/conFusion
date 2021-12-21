3ab263731f7e2772dbcf8faacfa86c762eb3269d
Giedrius <kalniusg@gmail.com>
2021-12-22 01:55:27 +0200

    Header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,10 +18,12 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   // cia deklrajuoja view class tai cia aprasomos directive, pipes ir components
-  declarations: [AppComponent, MenuComponent, DishdetailComponent],
+  declarations: [AppComponent, MenuComponent, DishdetailComponent, HeaderComponent, FooterComponent],
   // cia eina importai, nuo kuriu bus priklausoma musu aplikacija, kai importuojam jas reikia cia reikia jas aprasyti nuo ko bus dependant
   imports: [
     BrowserModule,
