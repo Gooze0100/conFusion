d3ae03f633d6fdc754b5a4afd699394898cb1678
Giedrius <kalniusg@gmail.com>
2021-12-22 16:20:41 +0200

    Angular router basics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { routes } from './routes';
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  // cia exportuojam kad app module galetu matyti routes ir tuo pasinaudoti
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,14 +16,27 @@ import { MatButtonModule } from '@angular/material/button';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
-import { DishdetailComponent } from './dishdetail/dishdetail.component';
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
+import { DishService } from './services/dish.service';
 
 @NgModule({
   // cia deklrajuoja view class tai cia aprasomos directive, pipes ir components
-  declarations: [AppComponent, MenuComponent, DishdetailComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+  ],
   // cia eina importai, nuo kuriu bus priklausoma musu aplikacija, kai importuojam jas reikia cia reikia jas aprasyti nuo ko bus dependant
   imports: [
     BrowserModule,
